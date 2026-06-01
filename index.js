d4e80e5d8dcbb40bade3dc0a16650d6a73ca9833
yeregurning <yeregurning134@gmail.com>
2026-06-01 23:41:33 +0700

    test webhook

--- index.js
+++ index.js
@@ -390,4 +390,5 @@ app.post('/webhook', async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log('🍌 Server HESA NLP berjalan di port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
