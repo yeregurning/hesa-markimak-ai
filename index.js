6de1eed0eb813bcbb389ccec9333659357d8ae14
yeregurning <yeregurning134@gmail.com>
2026-06-02 00:03:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -391,4 +391,6 @@ app.post('/webhook', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server berjalan di port ${PORT}`);
+});
